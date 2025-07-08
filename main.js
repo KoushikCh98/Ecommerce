8ca71b25bc3a599ef589eb701b4eb505bcfe2052
Koushik Choudhury <151964493+KoushikCh98@users.noreply.github.com>
2025-07-09 02:36:24 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-import './style.css'
+import "./style.css";
+
+import products from "./api/products.json";
+import { showProductContainer } from "./homeProductCards";
+
+//call to display the products
+showProductContainer(products);
