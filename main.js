380197eb530faa631500b95dc73afc7c1674ed5c
Koushik Choudhury <151964493+KoushikCh98@users.noreply.github.com>
2025-07-13 18:34:45 +0530

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-import "style.css";
-
-import products from "api/products.json";
-import { showProductContainer } from "homeProductCards";
-
-//call to display the products
-showProductContainer(products);
